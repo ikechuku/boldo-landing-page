e86a39588a1fbc05ff6220aec01f2f471d0986eb
ikechuku <sir_ik@yahoo.com>
2022-09-27 08:28:41 +0100

    chore: Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#0A2640',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#777777',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#65E4A3',
+      'lemon': '#69E6A6',
+      'black': '#000000',
+    }
   },
   plugins: [],
 }
