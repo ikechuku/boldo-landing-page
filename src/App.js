258802df2287b098191badf339f565626460666c
ikechuku <sir_ik@yahoo.com>
2022-09-27 08:29:21 +0100

    chore: Create a;plication components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/header';
+import Hero from './components/hero';
+import Services from "./components/services"
+import Footer from "./components/footer"
+import Testimonials from "./components/testimonials"
+import Subscribe from "./components/subscribe"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-">
+        <div className="app-container mx-auto">
+          <Header />
+          <Hero />
+        </div>
+      </div>
+      <Services />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
